79533b60f93c5128a36d8cb676f71d89f1eb5988
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-07-13 23:23:28 +0200

    another attempt

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,9 +1,6 @@
 import app from 'flarum/app';
-import { extend } from 'flarum/extend';
 import PostNotificationSettingsModal from './components/PostNotificationSettingsModal';
 
-app.initializers.add('tpokorra/post-notification', () => {
-	extend(PostNotificationSettingsModal.prototype, 'fields', function(items) {
-		items.add('p', <p>Your Text.</p>, -30);
-	});
+app.initializers.add('post-notification', () => {
+        app.extensionSettings['post-notification'] = () => app.modal.show(new PostNotificationSettingsModal());
 });
