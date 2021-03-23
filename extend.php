30c3e7d67475a3b9fbbe6b2ba4d4b7247462c239
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-03-23 21:03:40 +0100

    fix settings for Beta 16 (#11)

--- extend.php
+++ extend.php
@@ -11,12 +11,27 @@
 namespace tpokorra\PostNotification;
 
 use Flarum\Extend;
+use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\View\Factory;
 
 return [
     (new Extend\Frontend('admin'))
-            ->js(__DIR__.'/js/dist/admin.js'),
+        ->js(__DIR__.'/js/dist/admin.js'),
+
+    (new Extend\Settings())
+        ->serializeToForum('PostNotification.recipients.post_approval.to', 'PostNotification.recipients.post_approval.to')
+        ->serializeToForum('PostNotification.recipients.new_discussion.to', 'PostNotification.recipients.new_discussion.to')
+        ->serializeToForum('PostNotification.recipients.new_discussion.bcc', 'PostNotification.recipients.new_discussion.bcc')
+        ->serializeToForum('PostNotification.recipients.new_post.to', 'PostNotification.recipients.new_post.to')
+        ->serializeToForum('PostNotification.recipients.new_post.bcc', 'PostNotification.recipients.new_post.bcc')
+        ->serializeToForum('PostNotification.recipients.revised_post.to', 'PostNotification.recipients.revised_post.to')
+        ->serializeToForum('PostNotification.recipients.revised_post.bcc', 'PostNotification.recipients.revised_post.bcc')
+        ->serializeToForum('PostNotification.post_approval', 'PostNotification.post_approval')
+        ->serializeToForum('PostNotification.new_discussion', 'PostNotification.new_discussion')
+        ->serializeToForum('PostNotification.new_post', 'PostNotification.new_post')
+        ->serializeToForum('PostNotification.revised_post', 'PostNotification.revised_post'),
+
     new Extend\Locales(__DIR__.'/resources/locale'),
     function(Dispatcher $events, Factory $view) {
         $events->subscribe(Listeners\PostNotification::class);
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,70 @@
 import app from 'flarum/app';
-import PostNotificationSettingsModal from './components/PostNotificationSettingsModal';
+
+const translationPrefix = 'tpokorra-post-notification.admin.settings.';
+const settingsPrefix = 'PostNotification.';
+
 
 app.initializers.add('tpokorra-post-notification', () => {
-        app.extensionSettings['tpokorra-post-notification'] = () => app.modal.show(PostNotificationSettingsModal);
+    app.extensionData
+        .for('tpokorra-post-notification')
+
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.post_approval.to'),
+            setting: settingsPrefix + 'recipients.post_approval.to',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.new_discussion.to'),
+            setting: settingsPrefix + 'recipients.new_discussion.to',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.new_discussion.bcc'),
+            setting: settingsPrefix + 'recipients.new_discussion.bcc',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.new_post.to'),
+            setting: settingsPrefix + 'recipients.new_post.to',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.new_post.bcc'),
+            setting: settingsPrefix + 'recipients.new_post.bcc',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.revised_post.to'),
+            setting: settingsPrefix + 'recipients.revised_post.to',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'recipients.revised_post.bcc'),
+            setting: settingsPrefix + 'recipients.revised_post.bcc',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'post_approval'),
+            setting: settingsPrefix + 'post_approval',
+            type: 'text',
+            default: 'A new post by %s needs to be approved:',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'new_discussion'),
+            setting: settingsPrefix + 'new_discussion',
+            type: 'text',
+            default: 'A new discussion has been started by %s:',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'new_post'),
+            setting: settingsPrefix + 'new_post',
+            type: 'text',
+            default: 'A new message has been posted by %s:',
+        })
+        .registerSetting({
+            label: app.translator.trans(translationPrefix + 'revised_post'),
+            setting: settingsPrefix + 'revised_post',
+            type: 'text',
+            default: 'A message has been revised by %s:',
+        });
 });
